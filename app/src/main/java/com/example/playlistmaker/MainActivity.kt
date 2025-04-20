2176dd813a2ba68d7933300cd6be75c6e447f595
LampJuice <dieez347@yandex.ru>
2025-04-20 11:06:37 +0300

    Init commit

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +15,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        val search = findViewById<Button>(R.id.search_button)
+        val media = findViewById<Button>(R.id.media_button)
+        val settings = findViewById<Button>(R.id.settings_button)
+
+        val searchClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val searchIntent = Intent(this@MainActivity, SearchActivity::class.java)
+
+                startActivity(searchIntent)
+            }
+
+        }
+        search.setOnClickListener(searchClickListener)
+
+        media.setOnClickListener {
+            startActivity(Intent(this@MainActivity, MediatekaActivity::class.java))
+
+
+        }
+        settings.setOnClickListener { startActivity( Intent(this@MainActivity, SettingsActivity::class.java)) }
+
 
     }
 }
